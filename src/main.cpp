b3ca5cd709724858b340f5f84239f1b30e1fd9ef
martinmuszio <martinmuszio@gmail.com>
2022-07-08 13:31:14 -0300

    Clase 4 en master

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,26 @@
 #include <Arduino.h>
 
-// the setup function runs once when you press reset or power the board
-void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+// SENSOR DE DISTANCIA 
+#define TRIGGER 5 //Pin digital 2 para el TRIGGER del sensor
+#define ECHO    6 //Pin digital 3 para el ECHO del sensor
+
+
+void setup(){
+  Serial.begin (9600);
+  pinMode(TRIGGER, OUTPUT); //
+  pinMode(ECHO, INPUT); 
 }
 
-// the loop function runs over and over again forever
-void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+void loop(){   
+  long duracion, distancia ;
+  digitalWrite(TRIGGER, LOW);       // Nos aseguramos de que el trigger está desactivado
+  delayMicroseconds(2);             // Para asegurarnos de que el trigger esta LOW
+  digitalWrite(TRIGGER, HIGH);      // Activamos el pulso de salida
+  delayMicroseconds(10);            // Esperamos 10µs. El pulso sigue active este tiempo
+  digitalWrite(TRIGGER, LOW);       // Cortamos el pulso y a esperar el echo
+  duracion = pulseIn(ECHO, HIGH) ;  // guardamos la duracion del pulso
+  distancia = duracion / 58.2;      // Realizamos el calculo
+  Serial.print(distancia);          // enviamos la variable distancia
+  Serial.println(" cm.");           // y seguido los cm.
+  delay (500) ;                     // Para limitar el número de mediciones
 }
