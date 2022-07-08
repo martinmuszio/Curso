80b8b09849f0ae69f5129b228e78c13dab641c49
martinmuszio <martinmuszio@gmail.com>
2022-07-08 13:33:49 -0300

    Clase 5

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,44 @@
 #include <Arduino.h>
 
-// SENSOR DE DISTANCIA 
-#define TRIGGER 5 //Pin digital 2 para el TRIGGER del sensor
-#define ECHO    6 //Pin digital 3 para el ECHO del sensor
+/* CONTROL DE SERVO */
+#define PIN_SERVO A2  // PIN SIGNAL DEL SERVO a A2
 
 
-void setup(){
-  Serial.begin (9600);
-  pinMode(TRIGGER, OUTPUT); //
-  pinMode(ECHO, INPUT); 
+void setup(){ // Configuración inicial
+  
+  pinMode(PIN_SERVO, OUTPUT);   // Declara que PIN_SERVO será la Salida digital A2
+  digitalWrite(PIN_SERVO, LOW); // como de costumbre, incializamos en bajo
 }
 
-void loop(){   
-  long duracion, distancia ;
-  digitalWrite(TRIGGER, LOW);       // Nos aseguramos de que el trigger está desactivado
-  delayMicroseconds(2);             // Para asegurarnos de que el trigger esta LOW
-  digitalWrite(TRIGGER, HIGH);      // Activamos el pulso de salida
-  delayMicroseconds(10);            // Esperamos 10µs. El pulso sigue active este tiempo
-  digitalWrite(TRIGGER, LOW);       // Cortamos el pulso y a esperar el echo
-  duracion = pulseIn(ECHO, HIGH) ;  // guardamos la duracion del pulso
-  distancia = duracion / 58.2;      // Realizamos el calculo
-  Serial.print(distancia);          // enviamos la variable distancia
-  Serial.println(" cm.");           // y seguido los cm.
-  delay (500) ;                     // Para limitar el número de mediciones
+void loop(){
+
+  /* SERVO AL MEDIO (0°): debemos segun datasheet, enviar
+   * un pulso de 1.5 ms para que el servo quede en posicion
+   * de 0°
+  */
+  digitalWrite(PIN_SERVO, HIGH);  // subimos el pin 
+  delayMicroseconds(1500);        // durante 1500 us
+  digitalWrite(PIN_SERVO, LOW);   // bajamos el pin
+  delayMicroseconds(18500);       // durante el 18500 us
+  delay(2000);  //Esperamos 2 segundos para ver el movimiento
+
+  /* SERVO Derecha (90)°: debemos, segun datasheet, enviar
+   * un pulso de 2.0 ms para que el servo quede en posicion
+   * de 0°
+  */
+  digitalWrite(PIN_SERVO, HIGH);  // subimos el pin 
+  delayMicroseconds(2000);        // durante 1500 us
+  digitalWrite(PIN_SERVO, LOW);   // bajamos el pin
+  delayMicroseconds(18000);       // durante el 18000 us
+  delay(2000);  //Esperamos 2 segundos para ver el movimiento
+  
+  /* SERVO izquierda (-90°): debemos, segun datasheet, enviar
+   * un pulso de 1.0 ms para que el servo quede en posicion
+   * de 0°
+  */
+  digitalWrite(PIN_SERVO, HIGH);  // subimos el pin 
+  delayMicroseconds(1000);        // durante 2000 us
+  digitalWrite(PIN_SERVO, LOW);   // bajamos el pin
+  delayMicroseconds(19000);       // durante el 19000 us
+  delay(2000);  //Esperamos 2 segundos para ver el movimiento
 }
