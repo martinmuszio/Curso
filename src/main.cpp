cb552601065878805ef655c1a31a0d8940c68980
martinmuszio <martinmuszio@gmail.com>
2022-07-08 13:19:34 -0300

    Clase_2

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,17 @@
 #include <Arduino.h>
 
-// the setup function runs once when you press reset or power the board
+#define PIN_LED     5   // Led conectado a pin 5 que es salida PWM  
+#define PIN_ANALOG  A0  // el potenciometro lo conectamos al pin 
+
+
+int val = 0;    // variable para almacenar el analogico
+
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  // no es necesario nada, porque no son ni entrada, ni salida digital.
 }
 
-// the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+  val = analogRead(PIN_ANALOG);   // lee el potenciometro y el valor lo guarda el la variable val
+  val = val / 4;                  // Como el valor analogico es de 1024, y el PWM es de 256, dividimos el valor para fondo de escala 
+  analogWrite(PIN_LED, val);      // escribo PWM el valor val
 }
