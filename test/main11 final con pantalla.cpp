2521d8466d99d0dbd6a5eaf9691d6b31fb677cc1
martinmuszio <martinmuszio@gmail.com>
2022-07-28 19:08:36 -0300

    Tacometro, Cooler CPU

--- test/main11 final con pantalla.cpp
+++ test/main11 final con pantalla.cpp
@@ -1,12 +1,16 @@
 #include <Arduino.h>
 
+// liberia LCD
+#include <LiquidCrystal_I2C.h>
+
+// Clase LCD en direccion 0x27 de 16 caracteres x 2 renglones
+LiquidCrystal_I2C lcd(0x27,16,2);
+
 /* Para Tacometro*/
 float contador = 0; // guardamos la cuenta de giros
 int rpm = 0;        // el valor de rpm
 long ahora = 0;     // tiempo actual
 long antes = 0;     // tiempo pasado
-
-char buffer[20];
 
 // Funcion Interrpcion, solo aumenta la variable contador
 // es llamada en caso que attachInterrupt(pin, isr, modo) ocurra
@@ -20,13 +24,21 @@ void setup(){
   // inicializamos puerto serie
   Serial.begin(9600);
 
+  // Inicializacion de LCD
+  lcd.init();                   // inicio
+  lcd.backlight();              // prendemos el fondo
+  lcd.setCursor(0,0);           // posicionamos el cursor x=0 e y=0
+  lcd.print(" CURSO ROBOTICA"); // muestra en LCD
+  lcd.setCursor(3,1);           // posicionamos en x=3 e y y=1
+  lcd.print("PRACTICA 11");     // muestra LCD
+  delay(3000);
+
   // ahora habilitaremos la interrupcion, el cuales el pin fisico 2
   // la funcion digitalPinToInterrupt(2) devuelve el numero de interrupcion
   // que pertenece al pin 2, en este caso, devolveria e 0, a su vez
   // pasamos por parametro, la funcion que va a ejecutar cuando la interrupcion
   // ocurra, y el modo en que lo va a hacer, en este caso, puede ser Falling o rising
-  pinMode(2,INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(2), isr, FALLING); 
+  attachInterrupt(digitalPinToInterrupt(2), isr, RISING); 
 }
 
 void loop(){
@@ -36,7 +48,7 @@ void loop(){
   
   // si el tiempo actual menos el anterior, es mayor a 1000
   // significa que paso 1000 ms, lo cual es 1 segundo.
-  if((ahora - antes) >= 1000 ){
+  if((ahora - antes) > 1000 ){
     
     // en ese caso, actualizo el tiempo:
     antes = ahora;
@@ -49,18 +61,13 @@ void loop(){
 
     // a continuacion, simplemente mostraremos las RPM
     // en la pantalla
-    /*
-    Serial.print("Velocidad: ");
-    Serial.print(rpm);
-    Serial.print(" RPM");
-    Serial.print("\r");
-*/
-    Serial.println(rpm);
-    /*
-    sprintf (buffer, "Velocidad: %4d RPM\r",rpm);
-    Serial.print(buffer);
-  */
-
+    lcd.clear();  // borra toda la pantalla
+    lcd.setCursor(0,0);
+    lcd.print("TACOMETRO");
+    lcd.setCursor(0,1);
+    lcd.print(     rpm);
+    lcd.print(" RPM");
+    lcd.print("   ");
     // una vez mostrada la variable, reestablezco el contador
     // para que empiece a contar nuevamente de 0.
     contador = 0;
